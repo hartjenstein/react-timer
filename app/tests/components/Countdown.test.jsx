ea7d1b5d72ce67c7fc91af30bd9b99f3d489532c
Nick Hartjenstein <nick@iMac.local>
2017-03-27 10:21:23 +0200

    added countdown test and functionality

--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -10,4 +10,28 @@ describe('Countdown', () => {
     it('should exist', () => {
         expect(Countdown).toExist();
     });
+    // calling done let mocha know that we are planning to do an async test
+    // mocha will wait with the test completion until done is called
+    describe('handleSetCountdown', () => {
+        it('should set state to started and countdown', (done) => {
+            let countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(10);
+
+            expect(countdown.state.count).toBe(10);
+            expect(countdown.state.countdownStatus).toBe('started');
+            
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(9);
+            }, 1001);
+            done();
+        });
+        it('should stop count at zero', (done) => {
+            let countdown = TestUtils.renderIntoDocument (<Countdown/>);
+            countdown.handleSetCountdown(1);
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+            }, 3001);
+            done();
+        });
+    });    
 });
